7a0fd7885b667bbae010facfd3729f620a37c344
leokim0405 <48847503+leokim0405@users.noreply.github.com>
2021-02-05 15:45:31 +0900

    윈ㄷ도우 서버
    장단점, vm 스냅샷, java 기본

---
+++ java/workspace/java202102/src/example/TestEx01.java
@@ -0,0 +1,14 @@
+package example;
+
+public class TestEx01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello java");
+		System.out.println("hello ");
+		System.out.println("java");
+		
+
+	}
+
+}
---
+++ java/workspace/java202102/src/example/TestEx02.java
@@ -0,0 +1,11 @@
+package example;
+
+public class TestEx02 {
+
+	public static void main(String[] args) {
+		System.out.println("\\");
+		System.out.println("100" + 100);
+
+	}
+
+}
